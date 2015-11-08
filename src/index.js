1211b235de60cfa3d0ce8e8810231d362647f67b
Tadeusz Piskozub <tadeusz.piskozub@gmail.com>
2015-11-09 00:02:27 +0100

    Connect with Firebase, generate token.

--- src/index.js
+++ src/index.js
@@ -3,19 +3,37 @@ var request = require('request'),
     config  = require('./config.json'),
     Firebase = require('firebase'),
     bodyParser = require('body-parser'),
-    app  = express();
+    app  = express(),
+    ref = new Firebase(config.firebaseURL),
+    slackRef = ref.child('slack');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/notify', function(req, res) {
+ref.authWithCustomToken( config.firebaseToken, function(error) {
+  if (error) {
+    console.log('Authentication Failed!', error);
+  } else {
+    console.log('Authenticated successfully');
+  }
 
-  request.post({url:'https://slack.com/api/chat.postMessage', form: {
-    token: config.token,
-    channel: req.body.user,
-    text: req.body.text
-  }}, function(err, httpResponse, body){});
+  app.post('/token', function(req, res) {
+    var data = {};
+    data[req.body.uid] = Math.random().toString(36).replace(/^../, '');
 
-  res.end('ok');
+    slackRef.set(data);
+
+    res.end('ok');
+  });
+
+  app.post('/notify', function(req, res) {
+    request.post({url:'https://slack.com/api/chat.postMessage', form: {
+      token: config.slackToken,
+      channel: req.body.user,
+      text: req.body.text
+    }}, function(err, httpResponse, body){});
+
+    res.end('ok');
+  });
 });
 
 app.listen(8081);
