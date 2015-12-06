9c3104d75c0e946f8bdc52ef56eaf0a5e91b67d4
Tadeusz Piskozub <tadeusz.piskozub@gmail.com>
2015-12-06 22:02:32 +0100

    Add CORS, don't update token.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 var request = require('request'),
     express = require('express'),
     config  = require('./config.json'),
+    cors    = require('cors'),
     Firebase = require('firebase'),
     bodyParser = require('body-parser'),
     app  = express(),
     ref = new Firebase(config.firebaseURL),
     slackRef = ref.child('slack');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+app.use(bodyParser.json({extended: true}));
 
 ref.authWithCustomToken( config.firebaseToken, function(error) {
   if (error) {
@@ -16,36 +19,26 @@ ref.authWithCustomToken( config.firebaseToken, function(error) {
     console.log('Authenticated successfully');
   }
 
-  app.post('/token', function(req, res) {
-    res.end(updateToken(req.body.uid));
-  });
-
   app.post('/notify', function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     slackRef.child(req.body.uid).once('value', function(snapshot) {
 
       if (snapshot.val() == req.body.token) {
+        console.log('Channel: ' + req.body.user + ', text:' + req.body.text );
+
         request.post({url:'https://slack.com/api/chat.postMessage', form: {
           token: config.slackToken,
           channel: '@' + req.body.user,
           text: req.body.text
         }}, function(err, httpResponse, body){});
 
-        res.end(updateToken(req.body.uid));
+        res.end('ok');
       } else {
         res.end('invalid token');
       }
     });
   });
 });
-
-function updateToken(uid) {
-  var data = {};
-  data[uid] = Math.random().toString(36).replace(/^../, '');
-
-  slackRef.set(data);
-
-  return data[uid];
-}
 
 app.listen(8081);
